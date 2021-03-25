2ba2f3332d5872266d5991da51fb67fdc4c20c1d
thiago <ithiagolessa@gmail.com>
2021-03-25 16:55:18 -0300

    comeco da criacao do tr

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,36 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
 import Table from "./Table";
 import "./Formulario.css";
 
-const Formulario = () => {
-    return(
-        <div>
-            <form className="form-cadastro">
-                <div>
-                    <label htmlFor="name">Nome</label>
-                    <input id="name" type="text" name="nome" />
-                </div>
-                <div>
-                    <label htmlFor="date">Data de Nascimento</label>
-                    <input id="date" type="date" name="data" />
-                </div>
-                <div>
-                    <label htmlFor="state">Estado</label>
-                    <select id="state">
-                        <option>Selecione o estado</option>
-                    </select>
-                </div>
-                <div>
-                    <label htmlFor="city">Cidade</label>
-                    <select id="city" disabled>
-                        <option>Selecione a cidade</option>
-                    </select>
-                </div>
-                <button className="button-cadastro">Adicionar</button>
-            </form>
-            <Table />
-        </div>
-    )
+class Formulario extends Component {
+    
+    constructor() {
+        super();
+        this.state = {
+            pessoa: [
+                {
+                  nome: "Thiago",
+                  data: "01/01/1965",
+                  idade: 30,
+                  estado: "SP",
+                  cidade: "São paulo",
+                }
+              ]
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                <form className="form-cadastro">
+                    <div>
+                        <label htmlFor="name">Nome</label>
+                        <input id="name" type="text" name="nome" />
+                    </div>
+                    <div>
+                        <label htmlFor="date">Data de Nascimento</label>
+                        <input id="date" type="date" name="data" />
+                    </div>
+                    <div>
+                        <label htmlFor="state">Estado</label>
+                        <select id="state">
+                            <option>Selecione o estado</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="city">Cidade</label>
+                        <select id="city" disabled>
+                            <option>Selecione a cidade</option>
+                        </select>
+                    </div>
+                    <button className="button-cadastro">Adicionar</button>
+                </form>
+                <Table line={this.state.pessoa} />
+            </div>
+        )
+    }
 }
 
 export default Formulario;
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,35 +1,48 @@
 import React from "react";
 import "./Table.css";
 
-const Table = props => {
-    return(
-        <div>
-            <table className="tabela">
-                <thead>
-                    <tr>
-                        <th>Nome Completo</th>
-                        <th>Data</th>
-                        <th>Idade</th>
-                        <th>Estado</th>
-                        <th>Cidade</th>
-                        <th>Editar</th>
-                        <th>Excluir</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>José da Silva</td>
-                        <td>11/09/1995</td>
-                        <td>25</td>
-                        <td>RJ</td>
-                        <td>Rio de Janeiro</td>
-                        <td>editar</td>
-                        <td>excluir</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    )
-}
+const Table = (props) => {
+  return (
+    <div>
+      <table className="tabela">
+        <thead>
+          <tr>
+            <th>Nome Completo</th>
+            <th>Data</th>
+            <th>Idade</th>
+            <th>Estado</th>
+            <th>Cidade</th>
+            <th>Editar</th>
+            <th>Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>José da Silva</td>
+            <td>11/09/1995</td>
+            <td>25</td>
+            <td>RJ</td>
+            <td>Rio de Janeiro</td>
+            <td>editar</td>
+            <td>excluir</td>
+          </tr>
+          {props.line.map((itens, index) => {
+            return (
+              <tr key={index}>
+                <td>{itens.nome}</td>
+                <td>{itens.data}</td>
+                <td>{itens.idade}</td>
+                <td>{itens.estado}</td>
+                <td>{itens.cidade}</td>
+                <td>editar</td>
+                <td>excluir</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default Table;
